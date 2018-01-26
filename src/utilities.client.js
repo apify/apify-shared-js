3a127280a0fec82a705eb1e1a08d38ee68c40a0a
Marek Trunkat <marek@trunkat.eu>
2018-01-26 10:20:00 +0100

    Adding methods for url parsing and normalization

--- src/utilities.client.js
+++ src/utilities.client.js
@@ -69,3 +69,69 @@ export const getOrdinalSuffix = function (num) {
     const v = num % 100;
     return s[(v - 20) % 10] || s[v] || s[0];
 };
+
+export const parseUrl = (str) => {
+    if (typeof (str) !== 'string') { return {}; }
+    const o = {
+        strictMode: false,
+        key: ['source', 'protocol', 'authority', 'userInfo', 'user', 'password', 'host', 'port', 'relative', 'path', 'directory', 'file', 'query', 'fragment'],
+        q: {
+            name: 'queryKey',
+            parser: /(?:^|&)([^&=]*)=?([^&]*)/g,
+        },
+        parser: {
+            strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+            loose: /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/,
+        },
+    };
+    const m = o.parser[o.strictMode ? 'strict' : 'loose'].exec(str);
+    const uri = {};
+    let i = o.key.length;
+
+    while (i--) uri[o.key[i]] = m[i] || '';
+
+    uri[o.q.name] = {};
+    uri[o.key[12]].replace(o.q.parser, ($0, $1, $2) => {
+        if ($1) uri[o.q.name][$1] = $2;
+    });
+
+    // our extension - parse fragment using a query string format (i.e. "#key1=val1&key2=val2")
+    // this format is used by many websites
+    uri.fragmentKey = {};
+    if (uri.fragment) {
+        uri.fragment.replace(o.q.parser, ($0, $1, $2) => {
+            if ($1) uri.fragmentKey[$1] = $2;
+        });
+    }
+
+    return uri;
+};
+
+export const normalizeUrl = (url, keepFragment) => {
+    if (typeof url !== 'string' || !url.length) {
+        return null;
+    }
+
+    const urlObj = parseUrl(url.trim());
+    if (!urlObj.protocol || !urlObj.host) {
+        return null;
+    }
+
+    const path = urlObj.path.replace(/\/$/, '');
+    const params = (urlObj.query
+        ? urlObj.query
+            .split('&')
+            .filter((param) => {
+                return !/^utm_/.test(param);
+            })
+            .sort()
+        : []
+    );
+
+    return `${urlObj.protocol.trim().toLowerCase()
+    }://${
+        urlObj.host.trim().toLowerCase()
+    }${path.trim()
+    }${params.length ? `?${params.join('&').trim()}` : ''
+    }${keepFragment && urlObj.fragment ? `#${urlObj.fragment.trim()}` : ''}`;
+};
--- test/utilities.client.js
+++ test/utilities.client.js
@@ -43,4 +43,230 @@ describe('utilities.client', () => {
             }
         });
     });
+
+    describe('#normalizeUrl', () => {
+        it('should return null for invalid URLs', () => {
+            assert.equal(
+                utils.normalizeUrl('example.com'),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl('something'),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl('     127.0.0.1   '),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://'),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl('   http://'),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl('   http://     '),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl(''),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl('    '),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl(),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl(null),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl(undefined),
+                null,
+            );
+
+            assert.equal(
+                utils.normalizeUrl({}),
+                null,
+            );
+        });
+
+        it('should persist protocols', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com'),
+                'http://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('https://example.com'),
+                'https://example.com',
+            );
+        });
+
+        it('should lowercase hostname and protocols', () => {
+            assert.equal(
+                utils.normalizeUrl('httpS://EXAMPLE.cOm'),
+                'https://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('hTTp://www.EXAMPLE.com'),
+                'http://www.example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('SOMETHING://www.EXAMPLE.com/test'),
+                'something://www.example.com/test',
+            );
+        });
+
+        it('should remove trailing slash', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com'),
+                'http://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('ftp://example.com/'),
+                'ftp://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('anything://example.com/test/'),
+                'anything://example.com/test',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com/test.html'),
+                'http://example.com/test.html',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com/test.html/'),
+                'http://example.com/test.html',
+            );
+        });
+
+        it('should remove ? when empty QSA', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?'),
+                'http://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com/something?'),
+                'http://example.com/something',
+            );
+        });
+
+        it('should remove common tracking parameters', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?utm_source=xyz'),
+                'http://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?utm_campaign=xyz&param=val'),
+                'http://example.com?param=val',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?utm_campaign=xyz&utm_source=neco'),
+                'http://example.com',
+            );
+        });
+
+        it('should sort parameters alphabetically', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?a=a&b=b&c=c'),
+                'http://example.com?a=a&b=b&c=c',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?b=a&c=b&a=c'),
+                'http://example.com?a=c&b=a&c=b',
+            );
+        });
+
+        it('should handle keepFragment parameter correctly', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com#fragment', false),
+                'http://example.com',
+            );
+            assert.equal(
+                utils.normalizeUrl('http://example.com#', false),
+                'http://example.com',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://example.com#fragment', true),
+                'http://example.com#fragment',
+            );
+            assert.equal(
+                utils.normalizeUrl('http://example.com#', true),
+                'http://example.com',
+            );
+
+
+            assert.equal(
+                utils.normalizeUrl('https://www.example.com#keyB=val1&keyA=val2', true),
+                'https://www.example.com#keyB=val1&keyA=val2',
+            );
+            assert.equal(
+                utils.normalizeUrl('https://www.example.com#keyB=val1&keyA=val2', false),
+                'https://www.example.com',
+            );
+        });
+
+        it('should not touch invalid or empty params', () => {
+            assert.equal(
+                utils.normalizeUrl('http://example.com/?neco=&dalsi'),
+                'http://example.com?dalsi&neco=',
+            );
+        });
+
+        it('should normalize real-world URLs', () => {
+            assert.equal(
+                utils.normalizeUrl('https://www.czc.cz/dell-xps-15-9550-touch-stribrna_3/183874/produkt'
+                    + '?utm_source=heureka.cz'
+                    + '&utm_medium=cpc'
+                    + '&utm_campaign=Notebooky'
+                    + '&utm_term=Dell_XPS_15_9550_Touch_stribrna'),
+                'https://www.czc.cz/dell-xps-15-9550-touch-stribrna_3/183874/produkt',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('http://notebooky.heureka.cz/f:2111:25235;2278:9720,9539;p:579,580/'),
+                'http://notebooky.heureka.cz/f:2111:25235;2278:9720,9539;p:579,580',
+            );
+        });
+
+        it('should trim all parts of URL', () => {
+            assert.equal(
+                utils.normalizeUrl('    http    ://     test    # fragment   '),
+                'http://test',
+            );
+
+            assert.equal(
+                utils.normalizeUrl('    http   ://     test    # fragment   ', true),
+                'http://test#fragment',
+            );
+        });
+    });
 });
