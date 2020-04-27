bc576b8624470bdebe3bd80063dd8514171751d4
Misha <m.murasovs@gmail.com>
2020-04-27 11:37:19 +0200

    Add a renderer for ToC (doesn't generate anchor)

--- src/markdown_renderers.js
+++ src/markdown_renderers.js
@@ -33,3 +33,23 @@ export const customHeadingRenderer = (text, level, raw) => {
 
     return headingToReturn;
 };
+
+export const tocHeadingRenderer = (text, level, raw) => {
+    const idRegEx = /[^{}]+(?=})/g;
+    const idTags = text.match(idRegEx);
+    // If provided, format custom ID
+    let idTag = idTags && idTags.length ? formatIdTag(idTags[0]) : null;
+    // If no ID tag provided, generate from title
+    if (!idTag) idTag = formatIdTag(raw);
+    // If the custom ID is badly formatted, throw error
+    if (idTags && idTag !== idTags[0]) log.error('Badly formatted heading ID', { id: idTags[0] });
+
+    const titleText = raw.split('{')[0].trim();
+
+    const headingToReturn = `
+            <h${level} id="${idTag}">
+                ${titleText}
+            </h${level}>`;
+
+    return headingToReturn;
+};
