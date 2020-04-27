27a380674ded5f38f3ade138525d6a54843a21ef
Misha <m.murasovs@gmail.com>
2020-04-27 08:52:18 +0200

    Add comments and update code

--- src/markdown_renderers.js
+++ src/markdown_renderers.js
@@ -4,8 +4,8 @@ function formatIdTag(idTag) {
     // Get rid of whitespace and random characters
     idTag = idTag.toLowerCase().trim().replace(/[^\w]+/g, '-');
     // Remove dashes at the start
-    while (idTag[1] === '-') {
-        idTag = idTag.replace(idTag[1], '');
+    while (idTag[0] === '-') {
+        idTag = idTag.replace(idTag[0], '');
     }
     // Remove trailing dashes
     if (idTag.slice(-1) === '-') {
@@ -17,17 +17,18 @@ function formatIdTag(idTag) {
 export const customHeadingRenderer = (text, level, raw) => {
     const idRegEx = /[^{}]+(?=})/g;
     const idTags = text.match(idRegEx);
-
+    // If provided, format custom ID
     let idTag = idTags && idTags.length ? formatIdTag(idTags[0]) : null;
+    // If no ID tag provided, generate from title
     if (!idTag) idTag = formatIdTag(raw);
     // If the custom ID is badly formatted, throw error
     if (idTags && idTag !== idTags[0]) log.error('Badly formatted heading ID', { id: idTags[0] });
 
-    const titleText = text.split('{')[0].trim();
+    const titleText = raw.split('{')[0].trim();
 
     const headingToReturn = `
             <h${level} id="${idTag}">
-                <a href="#${idTag}"></a>
+                <a href="#${formatIdTag(titleText)}"></a>
                 ${titleText}
             </h${level}>`;
 
