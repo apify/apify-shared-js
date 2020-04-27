e2d19bf931ac09bd028498821e7cb5a9627c6a3d
Misha <m.murasovs@gmail.com>
2020-04-27 08:41:39 +0200

    Rewrite tests for edited rendering

--- test/markdown_renderers.js
+++ test/markdown_renderers.js
@@ -9,61 +9,29 @@ describe('customHeadingRenderer', () => {
     renderer.heading = customHeadingRenderer;
     marked.use({ renderer });
 
-    it('returns a header with ID param when ID is passed', () => {
-        const renderedTitle = function () {
-            return marked('# Welcome to Apify {#welcome-title-id}');
-        };
-        expect(renderedTitle()).to.equal(`
-            <h1>
-                <a 
-                    name="welcome-to-apify"
-                    href="#welcome-title-id"
-                    id="#welcome-title-id">
-                    <span class="header-link"></span>
-                </a>
+    it('uses the custom ID if provided', () => {
+        const renderedTitle = marked('# Welcome to Apify {welcome-title-id}');
+        expect(renderedTitle).to.equal(`
+            <h1 id="welcome-title-id">
+                <a href="#welcome-to-apify"></a>
                 Welcome to Apify
             </h1>`);
     });
 
-    it('adds a hashtag to ID if one isn\'t added', () => {
-        const renderedTitle = function () {
-            return marked('# Welcome to Apify {welcome-title-id}');
-        };
-        expect(renderedTitle()).to.equal(`
-            <h1>
-                <a 
-                    name="welcome-to-apify"
-                    href="#welcome-title-id"
-                    id="#welcome-title-id">
-                    <span class="header-link"></span>
-                </a>
-                Welcome to Apify
-            </h1>`);
-    });
-
-    it('returns a header without ID param when no ID is passed', () => {
-        const renderedTitle = marked('# Welcome to Apify');
+    it('generates ID from text if no ID provided', () => {
+        const renderedTitle = marked('## Welcome to Apify');
         expect(renderedTitle).to.eql(`
-            <h1>
-                <a 
-                    name="welcome-to-apify"
-                    href="#welcome-to-apify">
-                    <span class="header-link"></span>
-                </a>
+            <h2 id="welcome-to-apify">
+                <a href="#welcome-to-apify"></a>
                 Welcome to Apify
-            </h1>`);
+            </h2>`);
     });
 
     it('trims whitespace, inserts dashes between words, converts to lowercase, removes punctuation and trailing dash', () => {
         const renderedTitle = marked('# Welcome to Apify { #  .Welcome -title-id . - ? -}');
         expect(renderedTitle).to.eql(`
-            <h1>
-                <a 
-                    name="welcome-to-apify"
-                    href="#welcome-title-id"
-                    id="#welcome-title-id">
-                    <span class="header-link"></span>
-                </a>
+            <h1 id="welcome-title-id">
+                <a href="#welcome-to-apify"></a>
                 Welcome to Apify
             </h1>`);
     });
