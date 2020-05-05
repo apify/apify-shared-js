7b10655a17a3349fe5cbad0bb63b166bb7d176c0
Misha <m.murasovs@gmail.com>
2020-05-05 10:18:52 +0200

    Fix unwanted whitespace after title text

--- test/markdown_renderers.js
+++ test/markdown_renderers.js
@@ -12,24 +12,18 @@ describe('customHeadingRenderer', () => {
     it('uses the custom ID if provided', () => {
         const renderedTitle = marked('# Welcome to Apify {welcome-title-id}');
         expect(renderedTitle).to.equal(`
-            <h1 id="welcome-title-id">
-                Welcome to Apify
-            </h1>`);
+            <h1 id="welcome-title-id">Welcome to Apify</h1>`);
     });
 
     it('generates ID from text if no ID provided', () => {
         const renderedTitle = marked('## Welcome to Apify');
         expect(renderedTitle).to.eql(`
-            <h2 id="welcome-to-apify">
-                Welcome to Apify
-            </h2>`);
+            <h2 id="welcome-to-apify">Welcome to Apify</h2>`);
     });
 
     it('trims whitespace, inserts dashes between words, converts to lowercase, removes punctuation and trailing dash', () => {
         const renderedTitle = marked('# Welcome to Apify { #  .Welcome -title-id . - ? -}');
         expect(renderedTitle).to.eql(`
-            <h1 id="welcome-title-id">
-                Welcome to Apify
-            </h1>`);
+            <h1 id="welcome-title-id">Welcome to Apify</h1>`);
     });
 });
