dd4e1d5e675828b1261f5525cb7370ebd5188db9
Matej Hamas <matej.hamas@gmail.com>
2020-09-02 15:38:20 +0200

    Better custom marked

--- src/marked.js
+++ src/marked.js
@@ -1,12 +1,92 @@
-import { customCodeRenderer, customHeadingRenderer } from './markdown_renderers';
+import marked from 'marked';
+import { customHeadingRenderer } from './markdown_renderers';
 
-const marked = require('marked');
+const matchAll = require('match-all');
 
-export const overrideMarkedWithCustomSettings = () => {
+
+const APIFY_CODE_TABS = 'apify-code-tabs';
+const DEFAULT_MARKED_RENDERER = new marked.Renderer();
+
+/**
+ * @param {string} markdown
+ * @return { Object.<string, {language: string, code: string}> } tabs
+ */
+const codeTabObjectFromCodeTabMarkdown = (markdown) => {
+    const matchesIterator = matchAll(markdown, /<marked-tab header="(?<header>.*?)" lang="(?<lang>.*?)">(?<code>.*?)<\/marked-tab>/sg);
+    const matches = [];
+    let nextMatch = matchesIterator.nextRaw();
+    while (nextMatch) {
+        matches.push(nextMatch);
+        nextMatch = matchesIterator.nextRaw();
+    }
+
+    const tabs = {};
+    for (const match of matches) {
+        const { header, lang } = match.groups;
+        let tabCode = match.groups.code;
+
+        let numLeadingSpacesOnFirstLine = 0;
+        while (tabCode.startsWith('\n') || tabCode.startsWith(' ')) {
+            if (tabCode.startsWith('\n')) {
+                numLeadingSpacesOnFirstLine = 0;
+            } else if (tabCode.startsWith(' ')) {
+                numLeadingSpacesOnFirstLine += 1;
+            }
+            tabCode = tabCode.substring(1);
+        }
+
+        while (tabCode.endsWith('\n') || tabCode.endsWith(' ')) {
+            tabCode = tabCode.substring(0, tabCode.length - 1);
+        }
+
+        tabCode = tabCode
+            .split('\n')
+            .map((line, index) => {
+                if (index === 0) {
+                    return line;
+                }
+
+                for (let i = 0; i < numLeadingSpacesOnFirstLine; i++) {
+                    if (line.startsWith(' ')) {
+                        line = line.substring(1);
+                    }
+                }
+                return line;
+            })
+            .join('\n');
+        tabs[header] = { language: lang, code: tabCode };
+    }
+    return tabs;
+};
+
+
+/**
+ * @param {string} markdown
+ * @return {{ html: string, codeTabsObjectPerIndex: Object.<number, Object.<string, {language: string, code: string}>> }}
+ */
+export const apifyMarked = (markdown) => {
     const renderer = new marked.Renderer();
     renderer.heading = customHeadingRenderer;
-    renderer.code = customCodeRenderer;
-    marked.use({ renderer });
+    renderer.code = (code, language) => {
+        if (language === 'marked-tabs') {
+            return code;
+        }
+        return DEFAULT_MARKED_RENDERER.code(code, language);
+    };
+    const tokens = marked.lexer(markdown);
 
-    return marked;
+    let markedTabTokenIndex = 0;
+    const codeTabsObjectPerIndex = {};
+    tokens.forEach((token) => {
+        if (token.type === 'code' && token.lang && token.lang === 'marked-tabs') {
+            codeTabsObjectPerIndex[markedTabTokenIndex] = codeTabObjectFromCodeTabMarkdown(token.text);
+            token.text = `[${APIFY_CODE_TABS}]${markedTabTokenIndex}[${APIFY_CODE_TABS}]`;
+
+            markedTabTokenIndex++;
+        }
+    });
+
+    const html = marked.parser(tokens, { renderer });
+
+    return { html, codeTabsObjectPerIndex };
 };
--- test/marked.js
+++ test/marked.js
@@ -1,65 +1,83 @@
 /* eslint-disable */
 
 import { expect } from 'chai';
-import { overrideMarkedWithCustomSettings } from '../src/marked';
 
-describe('apifyMarked custom renderer works', () => {
-    const marked = overrideMarkedWithCustomSettings();
+import {apifyMarked} from '../src/marked';
 
-    it('uses the custom ID if provided', () => {
-        const renderedTitle = marked('# Welcome to Apify {welcome-title-id}');
-        expect(renderedTitle).to.equal(`
-            <h1 id="welcome-title-id">Welcome to Apify</h1>`);
-    });
+// This is here in order not to mess up indentation
+const MARKDOWN_UNDER_TEST = `
+# Title
 
-    it('generates ID from text if no ID provided', () => {
-        const renderedTitle = marked('## Welcome to Apify');
-        expect(renderedTitle).to.eql(`
-            <h2 id="welcome-to-apify">Welcome to Apify</h2>`);
-    });
+## Code block with tabs
+\`\`\`marked-tabs
+<marked-tab header="NodeJS" lang="javascript">
+console.log('Some JS code');
+</marked-tab>
 
-    it('trims whitespace, inserts dashes between words, converts to lowercase, removes punctuation and trailing dash', () => {
-        const renderedTitle = marked('# Welcome to Apify { #  .Welcome -title-id . - ? -}');
-        expect(renderedTitle).to.eql(`
-            <h1 id="welcome-title-id">Welcome to Apify</h1>`);
-    });
-
-    it('correctly parses code block with marked-tabs', () => {
-        const markdown = `# Welcome to Apify {welcome-title-id}
-            \`\`\`marked-tabs
-            <marked-tab header="NodeJS" lang="javascript">
-            console.log('Some JS code');
-            </marked-tab>
-            <marked-tab header="Python" lang="python">
-            print('Some python code');
-            count = 1
-            if count >= 1:
-                print('Some intended python code');
-            print('Some python code on next line');
-            </marked-tab>
-            <marked-tab header="Curl" lang="bash">
-            echo "Some bash code"
-            </marked-tab>
-            \`\`\`
-        `;
-
-        expect(marked(markdown).trim()).to.eql(`<h1 id="welcome-title-id">Welcome to Apify</h1><apify-code-tabs input="{
-\\"NodeJS\\": { \\"language\\": \\"javascript\\", \\"code: \\"console.log('Some JS code');\\" },
-\\"Python\\": { \\"language\\": \\"python\\", \\"code: \\"print('Some python code');
+<marked-tab header="Python" lang="python">
+print('Some python code');
 count = 1
 if count >= 1:
     print('Some intended python code');
-print('Some python code on next line');\\" },
-\\"Curl\\": { \\"language\\": \\"bash\\", \\"code: \\"echo \\"Some bash code\\"\\" }
-}" />`);
-    });
+print('Some python code on next line');
+</marked-tab>
 
-    it('correctly parses normal code block', () => {
-        const markdown = " # Test heading \
-        ``` \
-            console.log(\'Hello World\'); \
-        ```"
-        expect(marked(markdown)).to.eql(`
-            <h1 id="test-heading-console-log-hello-world">Test heading         <code>            console.log(&#39;Hello World&#39;);        </code></h1>`);
+<marked-tab header="Curl" lang="bash">
+echo "Some bash code"
+</marked-tab>
+\`\`\`
+
+## Code block without tabs
+\`\`\`javascript
+console.log('Your standard javascript code block')
+\`\`\`
+
+## Second block with tabs
+\`\`\`marked-tabs
+<marked-tab header="NodeJS" lang="javascript">
+console.log('Some JS code 2');
+</marked-tab>
+<marked-tab header="Curl" lang="bash">
+echo "Some bash code 2"
+</marked-tab>
+\`\`\`
+
+## Footer
+This is footer text.
+`;
+
+const EXPECTED_HTML =   '\n' +
+'            <h1 id="title">Title</h1>\n' +
+'            <h2 id="code-block-with-tabs">Code block with tabs</h2>[apify-code-tabs]0[apify-code-tabs]\n' +
+'            <h2 id="code-block-without-tabs">Code block without tabs</h2><pre><code class="language-javascript">console.log(&#39;Your standard javascript code block&#39;)</code></pre>\n' +
+'\n' +
+'            <h2 id="second-block-with-tabs">Second block with tabs</h2>[apify-code-tabs]1[apify-code-tabs]\n' +
+'            <h2 id="footer">Footer</h2><p>This is footer text.</p>\n';
+
+describe('apifyMarked custom renderer works', () => {
+
+    it('correctly parses markdown containing both ordinary code block and code blocks with tabs', () => {
+        const {html, codeTabsObjectPerIndex} = apifyMarked(MARKDOWN_UNDER_TEST);
+        expect(html).to.eql(EXPECTED_HTML);
+        expect(codeTabsObjectPerIndex).to.eql(
+            {
+                '0': {
+                  NodeJS: { language: 'javascript', code: "console.log('Some JS code');" },
+                  Python: {
+                    language: 'python',
+                    code: "print('Some python code');\n" +
+                      'count = 1\n' +
+                      'if count >= 1:\n' +
+                      "    print('Some intended python code');\n" +
+                      "print('Some python code on next line');"
+                  },
+                  Curl: { language: 'bash', code: 'echo "Some bash code"' }
+                },
+                '1': {
+                  NodeJS: { language: 'javascript', code: "console.log('Some JS code 2');" },
+                  Curl: { language: 'bash', code: 'echo "Some bash code 2"' }
+                }
+            }
+        );
     });
 });
