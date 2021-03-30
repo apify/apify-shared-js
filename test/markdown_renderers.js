ade3acf9a3ef0eba3c04fff2e0008b81833ebbe9
Misha <m.murasovs@gmail.com>
2021-03-30 15:04:16 +0200

    Add unit tests

--- test/markdown_renderers.js
+++ test/markdown_renderers.js
@@ -3,13 +3,15 @@
 import marked from 'marked';
 import { expect } from 'chai';
 
-import { customHeadingRenderer } from '../src/markdown_renderers';
+import { customHeadingRenderer, customLinkRenderer, customImageRenderer } from '../src/markdown_renderers';
 
 
-describe('apifyMarked custom renderer works', () => {
+describe('apifyMarked custom renderers work', () => {
     const renderer = new marked.Renderer();
     renderer.heading = customHeadingRenderer;
 
+    const branchName = 'main';
+    const repoFullName = 'apify/actor-test-url';
 
     it('uses the custom ID if provided', () => {
         const renderedTitle = marked('# [](#welcome-title-id) Welcome to Apify', { renderer });
@@ -28,4 +30,22 @@ describe('apifyMarked custom renderer works', () => {
         expect(renderedTitle).to.equal(`
             <h3 id="welcome-title-id"><a href="#welcome-title-id"></a>Welcome to Apify</h3>`);
     });
+
+    it('replaces relative URLs from GitHub repos with absolute URLs in links', () => {
+        const href = './src/foo/bar';
+        const text = 'link to bar';
+        const repoUrl = `https://github.com/${repoFullName}`;
+        const renderedLink = customLinkRenderer(href, text, repoUrl, branchName);
+
+        expect(renderedLink).to.equal('<a href=https://github.com/apify/actor-test-url/tree/main/src/foo/bar rel="nofollow noreferrer noopener">link to bar</a>')
+    })
+
+    it('replaces relative URLs in images from GitHub repos with absolute URLs pointing to raw files', () => {
+        const href = './src/foo/bar.jpeg'
+        const text = 'link to bar.jpeg';
+        const repoUrl = `https://gitlab.com/${repoFullName}`;
+        const renderedLink = customImageRenderer(href, text, repoUrl, branchName);
+
+        expect(renderedLink).to.equal(`<img src=https://gitlab.com/apify/actor-test-url/-/raw/main/src/foo/bar.jpeg alt=link to bar.jpeg />`)
+    })
 });
