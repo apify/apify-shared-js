1d8e101de912ecb3e4d7e32de34888b1c639658c
Martin Adámek <banan23@gmail.com>
2021-06-07 12:51:56 +0200

    docs: document logger based on SDK docs

--- packages/log/src/log.ts
+++ packages/log/src/log.ts
@@ -4,13 +4,26 @@ import { LogLevel, PREFIX_DELIMITER } from './log_consts';
 import { getLevelFromEnv, limitDepth } from './log_helpers';
 
 export interface LoggerOptions {
-    level: number;
-    maxDepth: number;
-    maxStringLength: number;
-    prefix: string | null;
-    suffix: string | null;
-    logger: Logger;
-    data: Record<string, unknown>,
+    /**
+     * Sets the log level to the given value, preventing messages from less important log levels
+     * from being printed to the console. Use in conjunction with the `log.LEVELS` constants.
+     */
+    level?: number;
+    /** Max depth of data object that will be logged. Anything deeper than the limit will be stripped off. */
+    maxDepth?: number;
+    /** Max length of the string to be logged. Longer strings will be truncated. */
+    maxStringLength?: number;
+    /** Prefix to be prepended the each logged line. */
+    prefix?: string | null;
+    /** Suffix that will be appended the each logged line. */
+    suffix?: string | null;
+    /**
+     * Logger implementation to be used. Default one is log.LoggerText to log messages as easily readable
+     * strings. Optionally you can use `log.LoggerJson` that formats each log line as a JSON.
+     */
+    logger?: Logger;
+    /** Additional data to be added to each log line. */
+    data?: Record<string, unknown>,
 }
 
 const getDefaultOptions = () => ({
@@ -23,10 +36,75 @@ const getDefaultOptions = () => ({
     data: {},
 });
 
+/**
+ * The log instance enables level aware logging of messages and we advise
+ * to use it instead of `console.log()` and its aliases in most development
+ * scenarios.
+ *
+ * A very useful use case for `log` is using `log.debug` liberally throughout
+ * the codebase to get useful logging messages only when appropriate log level is set
+ * and keeping the console tidy in production environments.
+ *
+ * The available logging levels are, in this order: `DEBUG`, `INFO`, `WARNING`, `ERROR`, `OFF`
+ * and can be referenced from the `log.LEVELS` constant, such as `log.LEVELS.ERROR`.
+ *
+ * To log messages to the system console, use the `log.level(message)` invocation,
+ * such as `log.debug('this is a debug message')`.
+ *
+ * To prevent writing of messages above a certain log level to the console, simply
+ * set the appropriate level. The default log level is `INFO`, which means that
+ * `DEBUG` messages will not be printed, unless enabled.
+ *
+ * **Example:**
+ * ```js
+ * const Apify = require('apify');
+ * const { log } = Apify.utils;
+ *
+ * log.info('Information message', { someData: 123 }); // prints message
+ * log.debug('Debug message', { debugData: 'hello' }); // doesn't print anything
+ *
+ * log.setLevel(log.LEVELS.DEBUG);
+ * log.debug('Debug message'); // prints message
+ *
+ * log.setLevel(log.LEVELS.ERROR);
+ * log.debug('Debug message'); // doesn't print anything
+ * log.info('Info message'); // doesn't print anything
+ *
+ * log.error('Error message', { errorDetails: 'This is bad!' }); // prints message
+ * try {
+ *   throw new Error('Not good!');
+ * } catch (e) {
+ *   log.exception(e, 'Exception occurred', { errorDetails: 'This is really bad!' }); // prints message
+ * }
+ *
+ * log.setOptions({ prefix: 'My actor' });
+ * log.info('I am running!'); // prints "My actor: I am running"
+ *
+ * const childLog = log.child({ prefix: 'Crawler' });
+ * log.info('I am crawling!'); // prints "My actor:Crawler: I am crawling"
+ * ```
+ *
+ * Another very useful way of setting the log level is by setting the `APIFY_LOG_LEVEL`
+ * environment variable, such as `APIFY_LOG_LEVEL=DEBUG`. This way, no code changes
+ * are necessary to turn on your debug messages and start debugging right away.
+ *
+ * To add timestamps to your logs, you can override the default logger settings:
+ * ```js
+ * log.setOptions({
+ *     logger: new log.LoggerText({ skipTime: false }),
+ * });
+ * ```
+ * You can customize your logging further by extending or replacing the default
+ * logger instances with your own implementations.
+ */
 export class Log {
+    /**
+     * Map of available log levels that's useful for easy setting of appropriate log levels.
+     * Each log level is represented internally by a number. Eg. `log.LEVELS.DEBUG === 5`.
+     */
     readonly LEVELS = LogLevel; // for BC
 
-    private options: LoggerOptions;
+    private options: Required<LoggerOptions>;
 
     private readonly deprecationsReported: Record<string, boolean> = {};
 
@@ -46,10 +124,26 @@ export class Log {
         return limitDepth(obj, this.options.maxDepth);
     }
 
+    /**
+     * Returns the currently selected logging level. This is useful for checking whether a message
+     * will actually be printed to the console before one actually performs a resource intensive operation
+     * to construct the message, such as querying a DB for some metadata that need to be added. If the log
+     * level is not high enough at the moment, it doesn't make sense to execute the query.
+     */
     getLevel() {
         return this.options.level;
     }
 
+    /**
+     * Sets the log level to the given value, preventing messages from less important log levels
+     * from being printed to the console. Use in conjunction with the `log.LEVELS` constants such as
+     *
+     * ```
+     * log.setLevel(log.LEVELS.DEBUG);
+     * ```
+     *
+     * Default log level is INFO.
+     */
     setLevel(level: LogLevel) {
         if (!LogLevel[level]) throw new Error('Options "level" must be one of log.LEVELS enum!');
 
@@ -69,14 +163,23 @@ export class Log {
         });
     }
 
+    /**
+     * Configures logger.
+     */
     setOptions(options: Partial<LoggerOptions>) {
         this.options = { ...this.options, ...options };
     }
 
-    getOptions() {
+    /**
+     * Returns the logger configuration.
+     */
+    getOptions(): Required<LoggerOptions> {
         return { ...this.options };
     }
 
+    /**
+     * Creates a new instance of logger that inherits settings from a parent logger.
+     */
     child(options: Partial<LoggerOptions>) {
         let { prefix } = this.options;
 
@@ -100,31 +203,52 @@ export class Log {
         return new Log(newOptions);
     }
 
-    error(message: string, data?: any) {
+    /**
+     * Logs an `ERROR` message. Use this method to log error messages that are not directly connected
+     * to an exception. For logging exceptions, use the `log.exception` method.
+     */
+    error(message: string, data?: Record<string, any>) {
         this.internal(LogLevel.ERROR, message, data);
     }
 
-    exception(exception: any, message: string, data?: any) {
+    /**
+     * Logs an `ERROR` level message with a nicely formatted exception. Note that the exception is the first parameter
+     * here and an additional message is only optional.
+     */
+    exception(exception: Error, message: string, data?: Record<string, any>) {
         this.internal(LogLevel.ERROR, message, data, exception);
     }
 
-    softFail(message: string, data?: any) {
+    softFail(message: string, data?: Record<string, any>) {
         this.internal(LogLevel.SOFT_FAIL, message, data);
     }
 
-    warning(message: string, data?: any) {
+    /**
+     * Logs a `WARNING` level message. Data are stringified and appended to the message.
+     */
+    warning(message: string, data?: Record<string, any>) {
         this.internal(LogLevel.WARNING, message, data);
     }
 
-    info(message: string, data?: any) {
+    /**
+     * Logs an `INFO` message. `INFO` is the default log level so info messages will be always logged,
+     * unless the log level is changed. Data are stringified and appended to the message.
+     */
+    info(message: string, data?: Record<string, any>) {
         this.internal(LogLevel.INFO, message, data);
     }
 
-    debug(message: string, data?: any) {
+    /**
+     * Logs a `DEBUG` message. By default, it will not be written to the console. To see `DEBUG`
+     * messages in the console, set the log level to `DEBUG` either using the `log.setLevel(log.LEVELS.DEBUG)`
+     * method or using the environment variable `APIFY_LOG_LEVEL=DEBUG`. Data are stringified and appended
+     * to the message.
+     */
+    debug(message: string, data?: Record<string, any>) {
         this.internal(LogLevel.DEBUG, message, data);
     }
 
-    perf(message: string, data?: any) {
+    perf(message: string, data?: Record<string, any>) {
         this.internal(LogLevel.PERF, message, data);
     }
 
