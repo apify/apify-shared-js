a6aae33dd893036967be7616cf797e9bd833237f
Matej Hamas <matej.hamas@apify.com>
2021-08-18 11:31:03 +0200

    feat: Generating CZK payment QR code (#245)

---
+++ packages/payment_qr_codes/src/czk_payment_qr_codes.ts
@@ -0,0 +1,27 @@
+import qrcode from 'qrcode';
+
+interface CzkQRCodeInputData {
+    iban: string;
+    amount: number;
+    currencyCode: string; // 3 letter currency code
+    message: string; // e.g. invoice number
+    beneficiaryName: string;
+}
+/*
+ * This function encodes provided data into a single string complying with CZK QR code payment protocol.
+ * The protocol is issued by Czech Bank Association and can be found
+ * at https://cbaonline.cz/upload/1645-standard-qr-v1-2-cerven-2021.pdf
+ */
+export function encodeInputDataToRawQrCodeInputString(data: CzkQRCodeInputData): string {
+    const { iban, amount, currencyCode, message, beneficiaryName } = data;
+    return `SPD*1.0*RN:${beneficiaryName}*ACC:${iban}*AM:${amount}*CC:${currencyCode.toUpperCase()}*MSG:${message}`;
+}
+
+/**
+ * This function generates CZK QR code that can be used for domestic QR code payment.
+ * It returns the QR code encoded as data URL that can be directly viewed in browser or used in <img> tag.
+ */
+export async function generateCzkPaymentQrCodeDataUrl(data: CzkQRCodeInputData): Promise<string> {
+    const code = qrcode.create(encodeInputDataToRawQrCodeInputString(data), {});
+    return qrcode.toDataURL(code.segments);
+}
---
+++ test/czk_payment_qr_codes.test.ts
@@ -0,0 +1,35 @@
+import {
+    encodeInputDataToRawQrCodeInputString,
+    generateCzkPaymentQrCodeDataUrl,
+} from '@apify/payment_qr_codes';
+
+describe('CZK payment QR code generation', () => {
+    it('can encode payment data to QR cod estring', () => {
+        expect(encodeInputDataToRawQrCodeInputString({
+            iban: 'CZ6508000000192000145399', // testing IBAN from https://www.cnb.cz/cs/platebni-styk/iban/iban-mezinarodni-format-cisla-uctu/
+            amount: 123.45,
+            currencyCode: 'czk',
+            message: 'Test message',
+            beneficiaryName: 'John Doe',
+        })).toBe('SPD*1.0*RN:John Doe*ACC:CZ6508000000192000145399*AM:123.45*CC:CZK*MSG:Test message');
+
+        expect(encodeInputDataToRawQrCodeInputString({
+            iban: 'CZ6508000000192000145399', // testing IBAN from https://www.cnb.cz/cs/platebni-styk/iban/iban-mezinarodni-format-cisla-uctu/
+            amount: 123.45,
+            currencyCode: 'EUR',
+            message: 'Test message',
+            beneficiaryName: 'John Doe',
+        })).toBe('SPD*1.0*RN:John Doe*ACC:CZ6508000000192000145399*AM:123.45*CC:EUR*MSG:Test message');
+    });
+
+    it('test', async () => {
+        const code = await generateCzkPaymentQrCodeDataUrl({
+            iban: 'CZ6508000000192000145399', // testing IBAN from https://www.cnb.cz/cs/platebni-styk/iban/iban-mezinarodni-format-cisla-uctu/
+            amount: 123.45,
+            currencyCode: 'czk',
+            message: 'Test message',
+            beneficiaryName: 'John Doe',
+        });
+        expect(code).toMatchSnapshot();
+    });
+});
