27adea95a3c1f0297a5c8b6f983f1306cb28d45d
Martin Adámek <banan23@gmail.com>
2021-11-19 17:11:11 +0100

    chore: add always passing test

--- test/timeout.test.ts
+++ test/timeout.test.ts
@@ -1,4 +1,8 @@
 describe('timeout with abort controller', () => {
+    it('empty', async () => {
+        // empty test so jest won't fail in node < 15 due to no tests being executed
+    });
+
     if (Number(process.version.match(/v(\d+)/)?.[1] ?? null) < 15) {
         // skip tests as this package requires node 15+
         return;
