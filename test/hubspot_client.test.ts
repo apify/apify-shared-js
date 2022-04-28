11f4fa86bd9ada07ec69101c9cce286e359e63bd
Jan Novotný <naj.yntovon@gmail.com>
2022-04-28 13:06:53 +0200

    feat(hubspot_client): add support for kanban url to contact (#293)
    * feat(hubspot_client): add support for kanban url to contact
    
    * feat(hubspot_client): fix test

--- test/hubspot_client.test.ts
+++ test/hubspot_client.test.ts
@@ -693,6 +693,7 @@ describe('HubspotClient', () => {
                         engagementType: 'SALES_REP_MEDIUM_INVOLVEMENT',
                     },
                 ],
+                kanbanUrl: 'https://console-securitybyobscurity.apify.com/orders/iJuo3Rvyg5LTbsB6F',
             };
 
             const contactId = 801;
@@ -710,6 +711,7 @@ describe('HubspotClient', () => {
                     lifecyclestage: 'customer',
                     segment_paying_customer: 'true',
                     subscription_plan: 'CUSTOM',
+                    apify_kanban_link_: 'https://console-securitybyobscurity.apify.com/orders/iJuo3Rvyg5LTbsB6F',
                 },
                 createdAt: '2021-04-18T21:23:09.435Z',
                 updatedAt: '2021-04-19T08:32:14.894Z',
