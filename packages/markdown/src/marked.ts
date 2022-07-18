e65a265f7dea8ad78629b4b7a03c4b14640a2fc6
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2022-07-18 12:30:21 +0200

    Add quote type to editorconfig

--- packages/markdown/src/marked.ts
+++ packages/markdown/src/marked.ts
@@ -1,6 +1,6 @@
-import { Renderer, lexer, parser } from "marked";
-import matchAll from "match-all";
-import { customHeadingRenderer } from "./markdown_renderers";
+import { Renderer, lexer, parser } from 'marked';
+import matchAll from 'match-all';
+import { customHeadingRenderer } from './markdown_renderers';
 
 /**
  * Map from the language of a fenced code block to the title of corresponding tab.
@@ -11,31 +11,31 @@ import { customHeadingRenderer } from "./markdown_renderers";
  * In case tab title can't be resolved from language using this mapping, the language itself is used as a tab title.
  */
 const LANGUAGE_TO_TAB_TITLE = {
-    js: "JavaScript",
-    javascript: "JavaScript",
-    ts: "TypeScript",
-    typescript: "TypeScript",
-    rs: "Rust",
-    rust: "Rust",
-    go: "Go",
-    golang: "Go",
-    nodejs: "Node.js",
-    bash: "Bash",
-    curl: "cURL",
-    dockerfile: "Dockerfile",
-    php: "PHP",
-    json: "JSON",
-    xml: "XML",
-    python: "Python",
-    python2: "Python 2",
-    python3: "Python 3",
-    yml: "YAML",
-    yaml: "YAML",
-    gql: "GraphQL",
-    graphql: "GraphQL",
+    js: 'JavaScript',
+    javascript: 'JavaScript',
+    ts: 'TypeScript',
+    typescript: 'TypeScript',
+    rs: 'Rust',
+    rust: 'Rust',
+    go: 'Go',
+    golang: 'Go',
+    nodejs: 'Node.js',
+    bash: 'Bash',
+    curl: 'cURL',
+    dockerfile: 'Dockerfile',
+    php: 'PHP',
+    json: 'JSON',
+    xml: 'XML',
+    python: 'Python',
+    python2: 'Python 2',
+    python3: 'Python 3',
+    yml: 'YAML',
+    yaml: 'YAML',
+    gql: 'GraphQL',
+    graphql: 'GraphQL',
 };
 
-const APIFY_CODE_TABS = "apify-code-tabs";
+const APIFY_CODE_TABS = 'apify-code-tabs';
 const DEFAULT_MARKED_RENDERER = new Renderer();
 
 interface Match {
@@ -160,8 +160,8 @@ export const apifyMarked = (markdown: string): MarkedResponse => {
     let markedTabTokenIndex = 0;
     const codeTabsObjectPerIndex = {};
     tokens.forEach((token) => {
-        if (token.type === "code" && token.lang) {
-            if (token.lang === "marked-tabs") {
+        if (token.type === 'code' && token.lang) {
+            if (token.lang === 'marked-tabs') {
                 codeTabsObjectPerIndex[markedTabTokenIndex] =
                     codeTabObjectFromCodeTabMarkdown(token.text);
             } else {
