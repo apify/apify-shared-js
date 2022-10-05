da56ee0635524db4212afeb0a93a3f0838efd7a6
drobnikj <drobnik.j@gmail.com>
2022-10-05 21:33:24 +0800

    feat(input_secrets): add new package to encrypt and decrypt input secrets

---
+++ packages/input_secrets/src/input_secrets.ts
@@ -0,0 +1,67 @@
+import ow from 'ow';
+import { KeyObject } from 'crypto';
+import { privateDecrypt, publicEncrypt } from '@apify/utilities';
+
+const BASE64_REGEXP = /[-A-Za-z0-9+/]*={0,3}/;
+const ENCRYPTED_INPUT_VALUE_PREFIX = 'ENCRYPTED_VALUE';
+const ENCRYPTED_INPUT_VALUE_REGEXP = new RegExp(`^${ENCRYPTED_INPUT_VALUE_PREFIX}:(${BASE64_REGEXP.source}):(${BASE64_REGEXP.source})$`);
+
+/**
+ * Get keys of secret fields from input schema
+ */
+export function getInputSchemaSecretFieldKeys(inputSchema: any): string[] {
+    return Object.keys(inputSchema.properties)
+        .filter((key) => !!inputSchema.properties[key].isSecret);
+}
+
+/**
+ * Encrypts actor input secrets
+ */
+export function encryptInputSecrets<T>(
+    { input, inputSchema, publicKey }: { input: T, inputSchema: object, publicKey: KeyObject},
+): T {
+    ow(input, ow.object);
+    ow(inputSchema, ow.object);
+    ow(publicKey, ow.object.instanceOf(KeyObject));
+
+    const secretsInInputKeys = getInputSchemaSecretFieldKeys(inputSchema);
+    if (secretsInInputKeys.length === 0) return input;
+
+    const encryptedInput = {};
+    for (const key of secretsInInputKeys) {
+        const value = input[key];
+        // NOTE: Skips already encrypted values. It can happens in case client already encrypted values, before
+        // sending them using API. Or input was takes from task, run console or scheduler, where input is stored encrypted.
+        if (value && ow.isValid(value, ow.string) && !ENCRYPTED_INPUT_VALUE_REGEXP.test(value)) {
+            const { encryptedValue, encryptedPassword } = publicEncrypt({ value: input[key], publicKey });
+            encryptedInput[key] = `${ENCRYPTED_INPUT_VALUE_PREFIX}:${encryptedPassword}:${encryptedValue}`;
+        }
+    }
+
+    return { ...input, ...encryptedInput };
+}
+
+/**
+ * Decrypts actor input secrets
+ * @param {Object} input
+ * @param {KeyObject} privateKey
+ * @returns Object
+ */
+export function decryptInputSecrets<T>(
+    { input, privateKey }: { input: T, privateKey: KeyObject },
+): T {
+    ow(input, ow.object);
+    ow(privateKey, ow.object.instanceOf(KeyObject));
+
+    const decryptedInput = {};
+    for (const [key, value] of Object.entries(input)) {
+        if (ow.isValid(value, ow.string) && ENCRYPTED_INPUT_VALUE_REGEXP.test(value)) {
+            const match = value.match(ENCRYPTED_INPUT_VALUE_REGEXP);
+            if (!match) continue;
+            const [, encryptedPassword, encryptedValue] = match;
+            decryptedInput[key] = privateDecrypt({ privateKey, encryptedPassword, encryptedValue });
+        }
+    }
+
+    return { ...input, ...decryptedInput };
+}
