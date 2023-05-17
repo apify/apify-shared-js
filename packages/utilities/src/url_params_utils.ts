add8539b56002054017840c99c243d0ccd02ee94
Josef Válek <valek.josef@seznam.cz>
2023-05-17 15:56:14 +0200

    feat(utils): use base64 encoding in url params utils

--- packages/utilities/src/url_params_utils.ts
+++ packages/utilities/src/url_params_utils.ts
@@ -1,3 +1,25 @@
+function base64urlToBase64(input: string) {
+    // Replace non-url compatible chars with base64 standard chars
+    input = input
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    // Pad out with standard base64 required padding characters
+    const pad = input.length % 4;
+    if (pad) {
+        if (pad === 1) {
+            throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+        }
+        input += new Array(5 - pad).join('=');
+    }
+
+    return input;
+}
+
+function base64ToBase64Url(input: string) {
+    return input.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/m, '');
+}
+
 /**
  * Encodes object (e.g. input for actor) to a string hash.
  */
@@ -5,14 +27,16 @@ export function encodeInput<T extends object>(input: T) {
     const data = JSON.stringify(input);
     const buffer = Buffer.from(data, 'utf8');
 
-    return buffer.toString('base64url');
+    const base64 = buffer.toString('base64');
+    return base64ToBase64Url(base64);
 }
 
 /**
  * Decodes a string hash produced via `encodeInput` back into the original object.
  */
 export function decodeInput(urlHash: string) {
-    const buffer = Buffer.from(urlHash, 'base64url');
+    const base64 = base64urlToBase64(urlHash);
+    const buffer = Buffer.from(base64, 'base64');
     const decoded = buffer.toString('utf8');
 
     return JSON.parse(decoded);
