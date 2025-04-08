bc5e57f46f1c5bec0d3234a08a9ace29254526c6
Josef Válek <valek.josef@seznam.cz>
2025-04-08 15:33:51 +0200

    feat: fileUrl as string editor type

--- test/input_schema.test.ts
+++ test/input_schema.test.ts
@@ -33,6 +33,12 @@ describe('input_schema.json', () => {
                         enum: ['a', 'b', 'c'],
                         enumTitles: ['A', 'B', 'C'],
                     },
+                    myField4: {
+                        title: 'Field title',
+                        type: 'string',
+                        description: 'Some description ...',
+                        editor: 'fileUrl',
+                    },
                 },
             };
 
@@ -157,7 +163,7 @@ describe('input_schema.json', () => {
 
             expect(() => validateInputSchema(validator, schema)).toThrow(
                 'Input schema is not valid (Field schema.properties.myField.editor must be equal to one of the allowed values: '
-                + '"javascript", "python", "textfield", "textarea", "hidden")',
+                + '"javascript", "python", "textfield", "textarea", "hidden", "fileUrl")',
             );
         });
 
