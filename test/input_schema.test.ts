0d62e98df5cee2546bf81a56b9628242b2157df1
martinforejt <forejt.martin97@gmail.com>
2025-10-16 12:17:52 +0200

    add test case

--- test/input_schema.test.ts
+++ test/input_schema.test.ts
@@ -1042,6 +1042,7 @@ describe('input_schema.json', () => {
                     '^(a*)*$',
                     '^(a+)?$',
                     '^(a*)?$',
+                    'a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*',
                 ];
 
                 for (const pattern of invalidRegexps) {
