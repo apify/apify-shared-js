78ae889103e589dd64f6d2082123febff0092a98
stepskop <stepan.skopek@apify.com>
2025-11-27 13:33:02 +0100

    chore: add missing as const for constants

--- packages/consts/src/consts.ts
+++ packages/consts/src/consts.ts
@@ -703,6 +703,6 @@ export const ACTOR_PERMISSION_LEVEL = {
      * For details refer to the Apify documentation.
      */
     LIMITED_PERMISSIONS: 'LIMITED_PERMISSIONS',
-};
+} as const;
 
 export type ACTOR_PERMISSION_LEVEL = ValueOf<typeof ACTOR_PERMISSION_LEVEL>;
